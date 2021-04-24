ba77a63e0b6cd1f59c36d780710e5cd18bb7ef6e
IbsanjU <bharath2668@gmail.com>
2021-04-24 17:56:19 +0530

    use Serialization

--- lib/code_runner.dart
+++ lib/code_runner.dart
@@ -51,11 +51,21 @@ class CodeRunner {
       ..brand = 'Mercedes'
       ..price = 10000
       ..passengerNames.addAll(['Bharath', 'Abhi', 'Radha', 'Lynda']));
-    print(car5);
-    // Copy and Modify vehicleType
-    var car6 = car5.rebuild((b) => b
-      ..type = VehicleType.motorbike
-      ..brand = 'Skoda Transportation');
-    print(car6);
+    // print(car5);
+    // // Copy and Modify vehicleType
+    // var car6 = car5.rebuild((b) => b
+    //   ..type = VehicleType.motorbike
+    //   ..brand = 'Skoda Transportation');
+    // print(car6);
+
+    // Convert car to Json
+    final car5Json = car5.toJson();
+    print(car5Json); // ["type","car","brand","Mercedes","price",10000.0,"passengerNames",["Bharath","Abhi","Radha","Lynda"]]
+
+
+    // Convert from Json
+    final car5FromJson =
+        BuiltVehicle.fromJson(car5Json).rebuild((b) => b..price = 500);
+    print(car5FromJson);
   }
 }
--- lib/model/serializers.dart
+++ lib/model/serializers.dart
@@ -5,10 +5,16 @@ import 'dart:convert';
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
+import 'package:built_value/standard_json_plugin.dart';
 import 'package:flutter_built_value/model/built_vehicle.dart';
 
 part 'serializers.g.dart';
 
-// annotate the top level field
+// #annotate the top level field
 @SerializersFor(const [BuiltVehicle, VehicleType])
-final Serializers serializers = _$serializers; // Create a Top level field
+final Serializers serializers = (_$serializers.toBuilder()
+  ..addPlugin(StandardJsonPlugin())).build(); // Create a Top level field
+
+// final Serializers serializers = _$serializers; // Create a Top level field
+// // #this will give json like
+// // #["type","car","brand","Mercedes","price",10000.0,"passengerNames",["Bharath","Abhi","Radha","Lynda"]]
